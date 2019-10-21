cf69b583c11be2265958a80ed70fe6071baedbea
remyli <rapi.remyli@gmail.com>
2019-10-21 09:59:04 +0900

    temp

---
+++ src/redux/reducers/visibilityFilter.js
@@ -0,0 +1,17 @@
+import { SET_FILTER } from "../actionTypes";
+import { VISIBILITY_FILTERS } from "../../constants";
+
+const initialiState = VISIBILITY_FILTERS;
+
+const visibilityFilter = (state = initialiState, action) => {
+  switch (action.type) {
+    case SET_FILTER: {
+      return action.payload.filter;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default visibilityFilter;
---
+++ src/redux/selectors.js
@@ -0,0 +1,13 @@
+export const getTodoList = store => {
+  store && store.todos ? store.todos.allIds : [];
+};
+
+export const getTodoById = (store, id) => {
+  store && store.todos && store.todos.byIds
+    ? { ...store.todos.byIds[id], id }
+    : {};
+};
+
+export const getTodos = store => {
+  getTodoList(store).map(id => getTodoList(store, id));
+};
