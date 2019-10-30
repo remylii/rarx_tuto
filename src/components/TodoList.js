4652228ddf23ad2b4ea0d00a54f0aee1bd4cd1d1
remyli <rapi.remyli@gmail.com>
2019-10-30 13:22:34 +0900

    use selectors

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Todo from "./Todo";
 import { connect } from "react-redux";
+import { getTodos } from "../redux/selectors";
 
 const TodoList = ({ todos }) => (
   <ul className="todo-list">
@@ -12,21 +13,6 @@ const TodoList = ({ todos }) => (
   </ul>
 );
 
-const mapStateToProps = state => {
-  const { byIds, allIds } = state.todos || {};
-  const todos =
-    allIds && allIds.length
-      ? allIds.map(id => {
-          return byIds
-            ? {
-                ...byIds[id],
-                id
-              }
-            : null;
-        })
-      : null;
-
-  return { todos };
-};
-
-export default connect(mapStateToProps)(TodoList);
+export default connect(state => ({
+  todos: getTodos(state)
+}))(TodoList);
